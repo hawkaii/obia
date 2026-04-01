2bbfe94fa0e92d75921bbd85d6590dcae1975427
Parthib Mukherjee <parthibmukherjee@gmail.com>
2026-04-01 20:09:38 +0530

    feat: add GoReleaser config, release workflow, and version flag
    Signed-off-by: Parthib Mukherjee <parthibmukherjee@gmail.com>

--- main.go
+++ main.go
@@ -7,9 +7,19 @@ import (
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/hawkaii/obia/internal/config"
 	"github.com/hawkaii/obia/internal/tui"
+	"github.com/hawkaii/obia/internal/version"
 )
 
 func main() {
+	if len(os.Args) > 1 && (os.Args[1] == "--version" || os.Args[1] == "-v") {
+		commit := version.Commit
+		if len(commit) > 7 {
+			commit = commit[:7]
+		}
+		fmt.Printf("obia %s (%s, %s)\n", version.Version, commit, version.Date)
+		return
+	}
+
 	cfg, err := config.Load()
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Error loading config: %v\n", err)
